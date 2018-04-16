0c7cf3eb2f7b19dee266ed72af3c4b99d40218e4
Serhii Savytsky <serhiisavytsky@Serhiis-MacBook-Pro.local>
2018-04-16 20:10:58 +0300

    playing around with chessboard

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.scss";
+
+import Chessboard from "./Chessboard";
 
 class App extends Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+
   render() {
     return (
       <div className="App">
@@ -10,6 +16,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <div>
+          <Chessboard />
+        </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
