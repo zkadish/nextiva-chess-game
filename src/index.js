2c0a803d42f5a6b529958b6fcf4281b7660eefb4
Zach Kadish <zach.kadish@nextiva.com>
2018-04-17 20:54:17 +0300

    added @nextiva/babel-config, and sass-loader to webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
 console.log('loaded');
+export const test1 = { test: 'test' };
+export const test2 = { ...test1 };
--- webpack.config.js
+++ webpack.config.js
@@ -5,23 +5,36 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'chess-game.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        use: 'babel-loader'
+        use: 'babel-loader',
       },
-    ]
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          {
+            loader: 'sass-loader',
+            options: {
+              includePaths: ['./src/sass'],
+            },
+          },
+        ],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
-    })
+    }),
   ],
   devServer: {
-    contentBase: path.resolve(__dirname, 'dist')
+    contentBase: path.resolve(__dirname, 'dist'),
   },
-  devtool: 'cheap-module-source-map'
-}
+  devtool: 'cheap-module-source-map',
+};
