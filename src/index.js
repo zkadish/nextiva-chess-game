8c6144fe793c98971412866429ea399b8bc9799a
Tymofii Savytskyi <savtym@gmail.com>
2018-04-18 18:11:11 +0300

    [MRG]

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import createSagaMiddleware from 'redux-saga'
 
-import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 import rootSaga from './saga'
