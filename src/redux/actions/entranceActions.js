ca406431c6a4df931a4dae5525230cd9789bd587
SirSerje <sir.serje@gmail.com>
2018-04-19 21:20:03 +0300

    action update

--- src/redux/actions/entranceActions.js
+++ src/redux/actions/entranceActions.js
@@ -1,19 +1,16 @@
 import { CREATE_ROOM, JOIN_ROOM, WATCH_ROOM, CREATE_ROOM_REQUEST, JOIN_ROOM_REQUEST, WATCH_ROOM_REQUEST, ROOMS_LIST } from "../constants/ActionTypes";
 import {ROUTE} from '../../redux/constants/route';
 
-//TODO: implement in component to dispatch on click handler
 export const createRoomRequest = () => ({
   type: CREATE_ROOM_REQUEST,
   payload: "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 });
 
-//TODO: implement in component to dispatch on click handler
 export const createJoinRequest = (room_id) => ({
   type: JOIN_ROOM_REQUEST,
   payload: room_id
 })
 
-//TODO: implement in component to dispatch on click handler
 export const createWatchRoomRequest = (room_id) => {
   return {
   type: WATCH_ROOM_REQUEST,
@@ -35,30 +32,12 @@ export const roomsList = (rooms) => ({
   payload: rooms
 });
 
-export const joinRoom = (counter, value) => dispatch => {
-  setTimeout(() => {dispatch(join());}, 500);
-};
+export const joinRoom = (payload) => ({
+  type:JOIN_ROOM,
+  payload,
+});
 
-export const watchRoom = (counter, value) => dispatch => {
-  setTimeout(() => {dispatch(watch());}, 500);
-};
-
-function watch(value) {
-  return {
-    type:WATCH_ROOM,
-    payload: value
-  }
-}
-function create(value) {
-  console.log('create', value)
-  return {
-    type:CREATE_ROOM,
-    payload: value
-  }
-}
-function join(value) {
-  return {
-    type:JOIN_ROOM,
-    payload: value
-  }
-}
+export const watchRoom = (payload) => ({
+  type:WATCH_ROOM,
+  payload,
+});
