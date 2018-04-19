07a4d2f781e647305ae8c33d46ecbea38a2627a6
SirSerje <sir.serje@gmail.com>
2018-04-19 15:36:41 +0300

    make connect in lobby

--- src/components/Lobby/Lobby.js
+++ src/components/Lobby/Lobby.js
@@ -3,7 +3,10 @@ import { Button } from '@nextiva/next-ui';
 
 import './lobby.scss';
 
-const Lobby = () => (
+import { createRoomRequest, createJoinRequest, createWatchRoomRequest } from "../../redux/actions/entranceActions";
+import { connect } from "react-redux";
+
+const Lobby = (props) => (
     <div className="lobby">
         <h3 className="lobby__description">All games</h3>
         <ul className="lobby__games">
@@ -24,10 +27,27 @@ const Lobby = () => (
             </li>
         </ul>
         <div className="lobby__action">
-            <Button kind="primary" className="lobby__action__join">Join</Button>
-            <Button kind="warning" className="lobby__action__watch">Watch</Button>
+            {/* TODO: unsure in buttons */}
+            <Button kind="primary" onClick={props.createJoinRequest} className="lobby__action__join">Join</Button>
+            <Button kind="success" onClick={props.createRoomRequest} className="lobby__action__join">Create</Button>
+            <Button kind="warning" onClick={props.createWatchRoomRequest} className="lobby__action__watch">Watch</Button>
         </div>
     </div>
 )
 
-export default Lobby;
+const mapStateToProps = state => {
+    return {
+        rooms: state.rooms        
+    };
+  };
+  
+  const mapDispatchToProps = dispatch => {
+    return {
+      createRoomRequest:        (params) => dispatch(createRoomRequest(params)),
+      createJoinRequest:        (params) => dispatch(createJoinRequest(params)),
+      createWatchRoomRequest:   (params) => dispatch(createWatchRoomRequest(params)),
+    };
+  };
+  
+  export default connect(mapStateToProps, mapDispatchToProps)(Lobby);
+  
